e4f43ace1a3e3c75313e1bb09d92fe83a5beb1a9
wongzit <zit.wong1995@gmail.com>
2025-09-20 17:32:05 +0000

    Deploying to gh-pages from @ wongzit/wongzit.github.io@2b69705e8289d96a248fe60e2a4f5c1f18540367 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -561,7 +561,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%77%61%6E%67.%7A%68%65.%64%72@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
         id: 'social-github',
@@ -569,13 +569,6 @@ ninja.data = [{
         section: 'Socials',
         handler: () => {
           window.open("https://github.com/wongzit", "_blank");
-        },
-      },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
         },
       },{
         id: 'social-orcid',
@@ -611,13 +604,6 @@ ninja.data = [{
         section: 'Socials',
         handler: () => {
           window.open("https://twitter.com/oooooootetsu", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
         },
       },{
       id: 'light-theme',
